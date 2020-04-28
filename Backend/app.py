ff360ed5f2e4d778dc2b7ec4d4b1e6bf3bc66372
Amirali Askari <askariamirali99@gmail.com>
2020-04-28 11:38:02 -0700

    Add the modules needed for the application to run.
    Create the infrastructure of the python application and enable the debug mode.

--- Backend/app.py
+++ Backend/app.py
@@ -1,7 +1,27 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+import urllib
+from sqlalchemy.orm import Session
+from sqlalchemy.ext.automap import automap_base
+from static.model.Model import Model
 
 app = Flask(__name__)
+params = urllib.parse.quote_plus(
+    "DRIVER={ODBC Driver 17 for SQL Server};SERVER=192.99.144.152;DATABASE=?????;"  # databasename instead of ???
+    "UID=????;PWD=????")  # username and password instead of ????
+app.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pyodbc:///?odbc_connect=%s" % params
+db = SQLAlchemy(app)
 
+engine = create_engine("mssql+pyodbc:///?odbc_connect=%s" % params)
+
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+session = Session(engine)
+model = Model(Base, session, db)
+
+## Routes of the application
 
 @app.route('/')
 def hello_world():
@@ -9,4 +29,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
