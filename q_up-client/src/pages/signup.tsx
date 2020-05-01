19120fd104152e3c5c95b7c75e3ec5a2bcc0a2ae
Karel Chanivecky <Karl_Chanivecky@hotmail.com>
2020-04-30 23:40:00 -0700

    setup skeleton login page

---
+++ q_up-client/src/pages/signup.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Footer from "src/components/static/Footer";
+import Header from "../components/static/Header";
+
+export default function LoginPage() {
+  return (
+    <>
+      <Header />
+      <main></main>
+      <Footer />
+    </>
+  );
+}
