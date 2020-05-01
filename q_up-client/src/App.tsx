e8b165962e7aad507fac31966d793422bb74fe82
TerryLun <tianweilun@yahoo.com>
2020-05-01 01:18:13 -0700

    created login page

--- q_up-client/src/App.tsx
+++ q_up-client/src/App.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import './App.css';
-import {
-  BrowserRouter as Router, Route, Switch
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './pages/landing';
 import LandingPage from './pages/landing';
+import LoginPage from './pages/login';
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route path='/' component={LandingPage} exact/>
-        </Switch>
-      </div>
-    </Router>
-  );
+   return (
+      <Router>
+         <div className='App'>
+            <Switch>
+               <Route path='/' component={LandingPage} exact />
+               <Route path='/login' component={LoginPage} exact />
+            </Switch>
+         </div>
+      </Router>
+   );
 }
 
 export default App;
--- q_up-client/src/pages/login.tsx
+++ q_up-client/src/pages/login.tsx
@@ -1,13 +1,58 @@
-import React from "react";
-import Footer from "src/components/static/Footer";
-import Header from "../components/static/Header";
+import React from 'react';
+import Footer from 'src/components/static/Footer';
+import Header from '../components/static/Header';
+import 'bootstrap/dist/css/bootstrap.css';
+import '../styles/loginPage.css';
 
 export default function LoginPage() {
-  return (
-    <>
-      <Header />
-      <main></main>
-      <Footer />
-    </>
-  );
+   return (
+      <>
+         <Header />
+         <main>
+            <div className='login-form first-element'>
+               <form action='/api/login' method='post'>
+                  <div className='form-group'>
+                     <input
+                        type='email'
+                        className='form-control'
+                        name='email'
+                        placeholder='Email'
+                        required
+                     />
+                  </div>
+                  <div className='form-group bottom-space'>
+                     <input
+                        type='password'
+                        className='form-control'
+                        name='password'
+                        placeholder='Password'
+                        required
+                     />
+                  </div>
+
+                  <div className='form-group'>
+                     <button
+                        type='submit'
+                        className='btn btn-success btn-lg btn-block'
+                     >
+                        Login
+                     </button>
+                  </div>
+                  <div className='form-group bottom-space'>
+                     <button
+                        type='button'
+                        className='btn btn-success btn-lg btn-block'
+                     >
+                        Signup
+                     </button>
+                  </div>
+               </form>
+               <div className='text-center last-element'>
+                  <a href='#'>Forgot user name password?</a>
+               </div>
+            </div>
+         </main>
+         <Footer />
+      </>
+   );
 }
