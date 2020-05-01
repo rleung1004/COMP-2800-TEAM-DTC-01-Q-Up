47e6cdb3fa056f7a86fa2eda986b9d7e4f745bc3
Karel Chanivecky <Karl_Chanivecky@hotmail.com>
2020-04-30 22:32:21 -0700

    Created a basic landing page skeleton

--- q_up-client/src/App.tsx
+++ q_up-client/src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router, Route, Switch
+} from 'react-router-dom';
+import './pages/landing';
+import LandingPage from './pages/landing';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path='/' component={LandingPage} exact/>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ q_up-client/src/pages/landing.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "../components/static/Header";
+import Footer from "src/components/static/Footer";
+
+export default function LandingPage() {
+  return (
+    <>
+      <Header />
+      <main>
+        <section>
+          <h2>The best is yet to come</h2>
+        </section>
+        <section>
+          <p>nothing but the best</p>
+          <p>nothing but the best</p>
+        </section>
+      </main>
+      <Footer />
+    </>
+  );
+}
