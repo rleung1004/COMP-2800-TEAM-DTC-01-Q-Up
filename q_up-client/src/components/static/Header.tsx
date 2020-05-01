8b04c00e5566ea8e9f5d0d940de56af83da16db5
Karel Chanivecky <Karl_Chanivecky@hotmail.com>
2020-04-30 23:34:26 -0700

    added a logo, added links to signup and login

--- q_up-client/src/components/static/Header.tsx
+++ q_up-client/src/components/static/Header.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-// import {Link} from 'react-router-dom';
+import {Link} from 'react-router-dom';
+import Grid from '@material-ui/core/Grid';
 
 export default function Header() {
+   
     return (
-    <header>
+    <header style={{backgroundColor: "gray"}}>
+        <Grid container justify="space-between" >
         <h1>QUP</h1>
+        <img src={require("../../img/temp_logo_png.png")} style={{height: 100, width: 100}} />
+        </Grid>
         <nav>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Signup</Link>
         </nav>
     </header>);
 }
