40818e4a1d145df732621d12dc9c8bb7a438690a
TerryLun <tianweilun@yahoo.com>
2020-05-01 01:45:23 -0700

    Linked Link to signup button.

--- q_up-client/src/pages/login.tsx
+++ q_up-client/src/pages/login.tsx
@@ -3,6 +3,7 @@ import Footer from 'src/components/static/Footer';
 import Header from '../components/static/Header';
 import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/loginPage.css';
+import { Link } from 'react-router-dom';
 
 export default function LoginPage() {
    return (
@@ -33,20 +34,20 @@ export default function LoginPage() {
                   <div className='form-group'>
                      <button
                         type='submit'
-                        className='btn btn-success btn-lg btn-block'
+                        className='btn white btn-lg btn-block'
                      >
                         Login
                      </button>
                   </div>
-                  <div className='form-group bottom-space'>
-                     <button
-                        type='button'
-                        className='btn btn-success btn-lg btn-block'
-                     >
-                        Signup
-                     </button>
-                  </div>
                </form>
+
+               <div className='form-group bottom-space'>
+                  <button type='button' className='btn btn-lg btn-block'>
+                     <Link className='signup-btn' to='/signup'>
+                        Signup
+                     </Link>
+                  </button>
+               </div>
                <div className='text-center last-element'>
                   <a href='#'>Forgot user name password?</a>
                </div>
