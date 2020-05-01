1b81ad3931dc25f7834cc755546ec40435c91ba8
Karel Chanivecky <Karl_Chanivecky@hotmail.com>
2020-05-01 02:13:49 -0700

    added pitch video and styling

--- q_up-client/src/pages/landing.tsx
+++ q_up-client/src/pages/landing.tsx
@@ -1,22 +1,49 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Header from "../components/static/Header";
 import Footer from "src/components/static/Footer";
-import '../styles/landing.scss';
+import "../styles/landing.scss";
+import { Grid, Button } from "@material-ui/core";
 
 export default function LandingPage() {
   return (
     <>
-      <Header/>
+      <Header />
       <main>
-        <section>
-          <h2>The best is yet to come</h2>
-        </section>
-        <section>
-          <p>nothing but the best</p>
-          <p>nothing but the best</p>
-        </section>
+        <Grid
+          container
+          justify="center"
+          alignItems="center"
+          className="top-pic-section"
+        >
+          <Grid item container direction="column" spacing={2}>
+            <Grid item>
+              <Link to="/login">
+                <Button variant="contained" className="btn">
+                  Login
+                </Button>
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link to="/signup">
+                <Button variant="contained" className="btn">
+                  Signup
+                </Button>
+              </Link>
+            </Grid>
+          </Grid>
+        </Grid>
       </main>
-      <Footer/>
+      <section id="iframe-container">
+        <iframe
+          id="pitch-video"
+          frameBorder="0"
+          src="https://www.youtube.com/embed/zUbBq3o2b2g"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </section>
+      <Footer />
     </>
   );
 }
