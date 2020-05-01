2b6028dec8e8808a7b864566c619e39e85c841cb
TerryLun <tianweilun@yahoo.com>
2020-05-01 13:19:31 -0700

    Now stores form data to database

--- q_up-firebase/functions/src/handlers/users.ts
+++ q_up-firebase/functions/src/handlers/users.ts
@@ -1,135 +1,185 @@
-import { db } from "../util/admin";
-import * as firebase from "firebase";
-import { firebaseConfig } from "../util/config";
-import { Request, Response } from "express";
-import { validateSignupData, validateLoginData } from "../util/validators";
+import { db } from '../util/admin';
+import * as firebase from 'firebase';
+import { firebaseConfig } from '../util/config';
+import { Request, Response } from 'express';
+import { validateSignupData, validateLoginData } from '../util/validators';
 
 firebase.initializeApp(firebaseConfig);
 
-const customerSignup = (req: Request, res: Response) => {
-  const newUser = {
-    email: req.body.email,
-    password: req.body.password,
-    confirmPassword: req.body.confirmPassword,
-  };
-  const { valid, errors } = validateSignupData(newUser);
+const Signup = (req: Request, res: Response) => {
+   const newUser = {
+      email: req.body.email,
+      password: req.body.password,
+      confirmPassword: req.body.confirmPassword,
+   };
 
-  if (!valid) {
-    return res.status(400).json(errors);
-  } else {
-    let token: string;
-    let userId: any;
+   const { valid, errors } = validateSignupData(newUser);
 
-    firebase
-      .auth()
-      .createUserWithEmailAndPassword(newUser.email, newUser.password)
-      .then((data) => {
-        userId = data.user?.uid;
-        return data.user?.getIdToken().then((generatedToken) => {
-          token = generatedToken;
-          const userCredentials = {
-            email: newUser.email,
-            createdAt: new Date().toISOString(),
-            accountType: "customer",
-            userId,
-          };
-          return db
-            .doc(`/users/${newUser.email}`)
-            .set(userCredentials)
-            .then(() => {
-              return res.status(201).json({ token });
+   if (!valid) {
+      return res.status(400).json(errors);
+   } else {
+      let token: string;
+      let userId: any;
+
+      firebase
+         .auth()
+         .createUserWithEmailAndPassword(newUser.email, newUser.password)
+         .then((data) => {
+            userId = data.user?.uid;
+            return data.user?.getIdToken().then((generatedToken) => {
+               token = generatedToken;
+               const userCredentials = {
+                  email: newUser.email,
+                  createdAt: new Date().toISOString(),
+                  userId,
+               };
+               return db
+                  .doc(`/users/${newUser.email}`)
+                  .set(userCredentials)
+                  .then(() => {
+                     return res.status(201).json({ token });
+                  });
             });
-        });
-      })
-      .catch((err) => {
-        console.error(err);
-        if (err.code === "auth/email-already-in-use") {
-          return res.status(400).json({ email: "Email is already in use" });
-        } else {
-          return res.status(500).json({ error: err.code });
-        }
-      });
-    return res.status(201);
-  }
+         })
+         .catch((err) => {
+            console.error(err);
+            if (err.code === 'auth/email-already-in-use') {
+               return res
+                  .status(400)
+                  .json({ email: 'Email is already in use' });
+            } else {
+               return res.status(500).json({ error: err.code });
+            }
+         });
+      return res.status(201);
+   }
 };
 
-const businessSignup = (req: Request, res: Response) => {
-  const newUser = {
-    email: req.body.email,
-    password: req.body.password,
-    confirmPassword: req.body.confirmPassword,
-  };
+// const customerSignup = (req: Request, res: Response) => {
+//   const newUser = {
+//     email: req.body.email,
+//     password: req.body.password,
+//     confirmPassword: req.body.confirmPassword,
+//   };
+//   const { valid, errors } = validateSignupData(newUser);
 
-  const { valid, errors } = validateSignupData(newUser);
+//   if (!valid) {
+//     return res.status(400).json(errors);
+//   } else {
+//     let token: string;
+//     let userId: any;
 
-  if (!valid) {
-    return res.status(400).json(errors);
-  } else {
-    let token: string;
-    let userId: any;
+//     firebase
+//       .auth()
+//       .createUserWithEmailAndPassword(newUser.email, newUser.password)
+//       .then((data) => {
+//         userId = data.user?.uid;
+//         return data.user?.getIdToken().then((generatedToken) => {
+//           token = generatedToken;
+//           const userCredentials = {
+//             email: newUser.email,
+//             createdAt: new Date().toISOString(),
+//             accountType: "customer",
+//             userId,
+//           };
+//           return db
+//             .doc(`/users/${newUser.email}`)
+//             .set(userCredentials)
+//             .then(() => {
+//               return res.status(201).json({ token });
+//             });
+//         });
+//       })
+//       .catch((err) => {
+//         console.error(err);
+//         if (err.code === "auth/email-already-in-use") {
+//           return res.status(400).json({ email: "Email is already in use" });
+//         } else {
+//           return res.status(500).json({ error: err.code });
+//         }
+//       });
+//     return res.status(201);
+//   }
+// };
 
-    firebase
-      .auth()
-      .createUserWithEmailAndPassword(newUser.email, newUser.password)
-      .then((data) => {
-        userId = data.user?.uid;
-        return data.user?.getIdToken().then((generatedToken) => {
-          token = generatedToken;
-          const userCredentials = {
-            email: newUser.email,
-            createdAt: new Date().toISOString(),
-            accountType: "business",
-            userId,
-          };
-          return db
-            .doc(`/users/${newUser.email}`)
-            .set(userCredentials)
-            .then(() => {
-              return res.status(201).json({ token });
-            });
-        });
-      })
-      .catch((err) => {
-        console.error(err);
-        if (err.code === "auth/email-already-in-use") {
-          return res.status(400).json({ email: "Email is already in use" });
-        } else {
-          return res.status(500).json({ error: err.code });
-        }
-      });
-    return res.status(201);
-  }
-};
+// const businessSignup = (req: Request, res: Response) => {
+//   const newUser = {
+//     email: req.body.email,
+//     password: req.body.password,
+//     confirmPassword: req.body.confirmPassword,
+//   };
+
+//   const { valid, errors } = validateSignupData(newUser);
+
+//   if (!valid) {
+//     return res.status(400).json(errors);
+//   } else {
+//     let token: string;
+//     let userId: any;
+
+//     firebase
+//       .auth()
+//       .createUserWithEmailAndPassword(newUser.email, newUser.password)
+//       .then((data) => {
+//         userId = data.user?.uid;
+//         return data.user?.getIdToken().then((generatedToken) => {
+//           token = generatedToken;
+//           const userCredentials = {
+//             email: newUser.email,
+//             createdAt: new Date().toISOString(),
+//             accountType: "business",
+//             userId,
+//           };
+//           return db
+//             .doc(`/users/${newUser.email}`)
+//             .set(userCredentials)
+//             .then(() => {
+//               return res.status(201).json({ token });
+//             });
+//         });
+//       })
+//       .catch((err) => {
+//         console.error(err);
+//         if (err.code === "auth/email-already-in-use") {
+//           return res.status(400).json({ email: "Email is already in use" });
+//         } else {
+//           return res.status(500).json({ error: err.code });
+//         }
+//       });
+//     return res.status(201);
+//   }
+// };
 
 const login = (req: Request, res: Response) => {
-  const user = {
-    email: req.body.email,
-    password: req.body.password,
-  };
-  const { valid, errors } = validateLoginData(user);
+   const user = {
+      email: req.body.email,
+      password: req.body.password,
+   };
+   const { valid, errors } = validateLoginData(user);
 
-  if (!valid) {
-    return res.status(400).json(errors);
-  } else {
-    firebase
-      .auth()
-      .signInWithEmailAndPassword(user.email, user.password)
-      .then((data) => {
-        return data.user?.getIdToken().then((generatedToken) => {
-          return res.status(200).json({ generatedToken });
-        });
-      })
-      .catch((err) => {
-        if (err.code === "auth/wrong-password") {
-          return res
-            .status(403)
-            .json({ general: "Wrong credentials, please try again" });
-        } else {
-          return res.status(500).json({ error: err.code });
-        }
-      });
-    return res.status(200);
-  }
+   if (!valid) {
+      return res.status(400).json(errors);
+   } else {
+      firebase
+         .auth()
+         .signInWithEmailAndPassword(user.email, user.password)
+         .then((data) => {
+            return data.user?.getIdToken().then((generatedToken) => {
+               return res.status(200).json({ generatedToken });
+            });
+         })
+         .catch((err) => {
+            if (err.code === 'auth/wrong-password') {
+               return res
+                  .status(403)
+                  .json({ general: 'Wrong credentials, please try again' });
+            } else {
+               return res.status(500).json({ error: err.code });
+            }
+         });
+      return res.status(200);
+   }
 };
 
-export { customerSignup, businessSignup, login };
+export { Signup, login };
+// export { customerSignup, businessSignup, login };
--- q_up-firebase/functions/src/index.ts
+++ q_up-firebase/functions/src/index.ts
@@ -1,22 +1,23 @@
-import * as functions from "firebase-functions";
-import { getQueues, enterQueue } from "./handlers/queues";
-import * as express from "express";
-import { customerSignup, login, businessSignup } from "./handlers/users";
-import { FBAuth } from "./util/fbAuth";
+import * as functions from 'firebase-functions';
+import { getQueues, enterQueue } from './handlers/queues';
+import * as express from 'express';
+// import { customerSignup, login, businessSignup } from './handlers/users';
+import { Signup, login } from './handlers/users';
+import { FBAuth } from './util/fbAuth';
 
 const app = express();
 
 // all routes start with https://us-central1-q-up-c2b70.cloudfunctions.net/api
 
 // queue routes
-app.get("/getQueue", getQueues);
-app.post("/enterQueue", FBAuth, enterQueue);
+app.get('/getQueue', getQueues);
+app.post('/enterQueue', FBAuth, enterQueue);
 
 // customer signup route
-app.post("/customer/signup", customerSignup);
-// Business sign up
-app.post("/business/signup", businessSignup);
+app.post('/signup', Signup);
+// // Business sign up
+// app.post("/business/signup", businessSignup);
 // login route
-app.post("/login", login);
+app.post('/login', login);
 
 exports.api = functions.https.onRequest(app);
