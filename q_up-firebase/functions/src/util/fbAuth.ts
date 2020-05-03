4d620e426392e0958036efd6b9f0a0f90062a487
TerryLun <tianweilun@yahoo.com>
2020-05-03 01:15:33 -0700

    Rename accountType to userType

--- q_up-firebase/functions/src/util/fbAuth.ts
+++ q_up-firebase/functions/src/util/fbAuth.ts
@@ -1,38 +1,38 @@
-import { admin, db } from "../util/admin";
-import { Request, Response } from "express";
+import { admin, db } from '../util/admin';
+import { Request, Response } from 'express';
 
 export const FBAuth = (req: Request, res: Response, next: Function) => {
-  let idToken: string;
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer ")
-  ) {
-    idToken = req.headers.authorization.split("Bearer ")[1];
-  } else {
-    console.error("No token found");
-    return res.status(403).json({ error: "Unauthorized" });
-  }
+   let idToken: string;
+   if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith('Bearer ')
+   ) {
+      idToken = req.headers.authorization.split('Bearer ')[1];
+   } else {
+      console.error('No token found');
+      return res.status(403).json({ error: 'Unauthorized' });
+   }
 
-  admin
-    .auth()
-    .verifyIdToken(idToken)
-    .then((decodedToken) => {
-      Object.assign(req.body, { decodedToken });
-      return db
-        .collection("users")
-        .where("userId", "==", req.body.decodedToken.uid)
-        .limit(1)
-        .get()
-        .then((data) => {
-          let userEmail = data.docs[0].data().email;
-          let accountType = data.docs[0].data().accountType;
-          Object.assign(req.body, { userEmail, accountType });
-          return next();
-        });
-    })
-    .catch((err) => {
-      console.error("Error while verifying token", err);
-      return res.status(403).json(err);
-    });
-  return undefined;
+   admin
+      .auth()
+      .verifyIdToken(idToken)
+      .then((decodedToken) => {
+         Object.assign(req.body, { decodedToken });
+         return db
+            .collection('users')
+            .where('userId', '==', req.body.decodedToken.uid)
+            .limit(1)
+            .get()
+            .then((data) => {
+               let userEmail = data.docs[0].data().email;
+               let userType = data.docs[0].data().userType;
+               Object.assign(req.body, { userEmail, userType });
+               return next();
+            });
+      })
+      .catch((err) => {
+         console.error('Error while verifying token', err);
+         return res.status(403).json(err);
+      });
+   return undefined;
 };
