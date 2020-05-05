e856e4e9521fe15012682ab1e52a3570de368a63
Karel Chanivecky <Karl_Chanivecky@hotmail.com>
2020-05-05 11:42:15 -0700

    added  header

---
+++ q_up-client/src/pages/clientRegistration.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+import Footer from 'src/components/static/Footer';
+import Header from 'src/components/static/Header';
+
+export default function ClientRegistrationPage() {
+    return <>
+    <Header nav="mock nav"/>
+        <main>
+        </main>
+    <Footer/>
+    </>;
+}
