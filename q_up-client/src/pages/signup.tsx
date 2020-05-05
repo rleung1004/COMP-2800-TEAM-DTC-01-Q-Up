5f43e9669fcb697c48a28e4ffce2ff2b3edbc616
Karel Chanivecky <Karl_Chanivecky@hotmail.com>
2020-05-05 16:16:06 -0700

    implemented state, renamed function

--- q_up-client/src/pages/signup.tsx
+++ q_up-client/src/pages/signup.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import Footer from "src/components/static/Footer";
 import Header from "../components/static/Header";
 import "bootstrap/dist/css/bootstrap.css";
@@ -16,8 +16,14 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function LoginPage() {
+export default function signupPage() {
   const classes = useStyles();
+  const [formState, setFormState] = useState({password: '', username: '', confirmPass:''}); 
+  const handleOnFieldChange = (event: any) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setFormState(prevState=>({...prevState, [name]: value}));
+  };
   return (
     <>
       <Header />
@@ -28,18 +34,27 @@ export default function LoginPage() {
               required
               id="standard-required"
               label="Username"
+              name="username"
+              onChange={handleOnFieldChange}
+              value={formState.username}
             />
             <TextField
               required
               id="standard-password-input"
               label="Password"
+              name="password"
               type="password"
+              onChange={handleOnFieldChange}
+              value={formState.password}
             />
             <TextField
             required
               id="standard-password-input"
               label="Confirm password"
+              name="confirmPass"
               type="password"
+              onChange={handleOnFieldChange}
+              value={formState.confirmPass}
             />
           </form>
 
