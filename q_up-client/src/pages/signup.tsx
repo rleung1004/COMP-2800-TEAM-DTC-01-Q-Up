12f9627326def786afec556513e6f5aaae91cbca
Karel Chanivecky <Karl_Chanivecky@hotmail.com>
2020-05-05 15:17:26 -0700

    changed to material ui components

--- q_up-client/src/pages/signup.tsx
+++ q_up-client/src/pages/signup.tsx
@@ -1,94 +1,59 @@
-import React from 'react';
-import Footer from 'src/components/static/Footer';
-import Header from '../components/static/Header';
-import 'bootstrap/dist/css/bootstrap.css';
-import '../styles/signupPage.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import Footer from "src/components/static/Footer";
+import Header from "../components/static/Header";
+import "bootstrap/dist/css/bootstrap.css";
+import "../styles/signupPage.css";
+import { Link } from "react-router-dom";
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
+      width: "25ch",
+    },
+  },
+}));
 
 export default function LoginPage() {
-   return (
-      <>
-         <Header />
-         <main>
-            <div className='login-form first-element'>
-               <form
-                  action='https://us-central1-q-up-c2b70.cloudfunctions.net/api/signup'
-                  method='post'
-               >
-                  <div className='form-group'>
-                     <input
-                        type='email'
-                        className='form-control'
-                        name='email'
-                        placeholder='Email'
-                        required
-                     />
-                  </div>
+  const classes = useStyles();
+  return (
+    <>
+      <Header />
+      <main>
+        <div className="login-form first-element">
+          <form className={classes.root} noValidate autoComplete="off">
+            <TextField
+              required
+              id="standard-required"
+              label="Username"
+            />
+            <TextField
+              required
+              id="standard-password-input"
+              label="Password"
+              type="password"
+            />
+            <TextField
+            required
+              id="standard-password-input"
+              label="Confirm password"
+              type="password"
+            />
+          </form>
 
-                  <div className='form-group'>
-                     <input
-                        type='password'
-                        className='form-control'
-                        name='password'
-                        placeholder='Password'
-                        required
-                     />
-                  </div>
-
-                  <div className='form-group bottom-space'>
-                     <input
-                        type='password'
-                        className='form-control'
-                        name='confirmPassword'
-                        placeholder='Re-enter Password'
-                        required
-                     />
-                  </div>
-
-                  <div className='radio-class'>
-                     <label>
-                        <input
-                           type='radio'
-                           id='user'
-                           name='userType'
-                           value='customer'
-                        />
-                        I want to queue as a customer.
-                     </label>
-                  </div>
-                  <div className='radio-class bottom-space'>
-                     <label>
-                        <input
-                           type='radio'
-                           id='business'
-                           name='userType'
-                           value='business'
-                           required
-                        />
-                        I am a manager of a business.
-                     </label>
-                  </div>
-                  <div className='form-group'>
-                     <button
-                        type='submit'
-                        className='btn white btn-lg btn-block'
-                     >
-                        Signup
-                     </button>
-                  </div>
-               </form>
-
-               <div className='form-group bottom-space'>
-                  <button type='button' className='btn btn-lg btn-block'>
-                     Back
-                  </button>
-               </div>
-               <div className='text-center last-element'>
-                  Already have an account? <Link to='/login'>Log in</Link>
-               </div>
-            </div>
-         </main>
-         <Footer />
-      </>
-   );
+          <div className="form-group bottom-space">
+            <button type="button" className="btn btn-lg btn-block">
+              Back
+            </button>
+          </div>
+          <div className="text-center last-element">
+            Already have an account? <Link to="/login">Log in</Link>
+          </div>
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
 }
