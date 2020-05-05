6d8b97590e9981284ba9918a5f718cfea09d4a87
Karel Chanivecky <Karl_Chanivecky@hotmail.com>
2020-05-05 12:06:30 -0700

    added nav bar to header

--- q_up-client/src/pages/clientRegistration.tsx
+++ q_up-client/src/pages/clientRegistration.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 // import { Link } from 'react-router-dom';
 import Footer from 'src/components/static/Footer';
 import Header from 'src/components/static/Header';
+import ConsumerNav from 'src/components/consumerNav';
 
 export default function ClientRegistrationPage() {
     return <>
-    <Header nav="mock nav"/>
+    <Header/>
         <main>
         </main>
     <Footer/>
