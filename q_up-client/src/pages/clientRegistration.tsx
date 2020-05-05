6f1bdb037968b72afe6833afa3ade445861b8260
Karel Chanivecky <Karl_Chanivecky@hotmail.com>
2020-05-05 10:56:21 -0700

    init client registration page

---
+++ q_up-client/src/pages/clientRegistration.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+import Footer from 'src/components/static/Footer';
+
+export default function ClientRegistrationPage(props) {
+    return <>
+        <main>
+        </main>
+        <Footer/>
+    </>;
+}
