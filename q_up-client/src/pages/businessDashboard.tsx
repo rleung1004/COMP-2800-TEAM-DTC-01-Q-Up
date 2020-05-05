2e5e9c4d28315a990345406e6490309929a7ab9b
Karel Chanivecky <Karl_Chanivecky@hotmail.com>
2020-05-05 12:28:51 -0700

    init business dashboard

---
+++ q_up-client/src/pages/businessDashboard.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+import Footer from 'src/components/static/Footer';
+import Header from 'src/components/static/Header';
+import ConsumerNav from 'src/components/consumerNav';
+
+export default function ClientDashboardPage() {
+    return <>
+    <Header Nav={ConsumerNav}/>
+        <main>
+            business dashboard
+        </main>
+    <Footer/>
+    </>;
+}
