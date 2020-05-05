061e74d429f85b92184b819268b4efc5064a7834
Karel Chanivecky <Karl_Chanivecky@hotmail.com>
2020-05-05 12:06:48 -0700

    removed unused import

--- q_up-client/src/pages/clientRegistration.tsx
+++ q_up-client/src/pages/clientRegistration.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 // import { Link } from 'react-router-dom';
 import Footer from 'src/components/static/Footer';
 import Header from 'src/components/static/Header';
-import ConsumerNav from 'src/components/consumerNav';
 
 export default function ClientRegistrationPage() {
     return <>
