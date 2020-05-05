4b5717e69aafc68e1a83cd5c35b3f7dc2abf2cee
Karel Chanivecky <Karl_Chanivecky@hotmail.com>
2020-05-05 13:37:34 -0700

    init about us page

---
+++ q_up-client/src/pages/aboutUs.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Footer from 'src/components/static/Footer';
+import Header from 'src/components/static/Header';
+
+export default function aboutUsPage() {
+    return <>
+    <Header/>
+        <main>
+            about us page
+        </main>
+    <Footer/>
+    </>;
+}
