26e1d863c62c9944e9fc9377446139a92ee6235c
TerryLun <tianweilun@yahoo.com>
2020-05-05 23:37:02 -0700

    Changed to NavLink tag for about us page

--- q_up-client/src/components/static/Footer.tsx
+++ q_up-client/src/components/static/Footer.tsx
@@ -1,36 +1,83 @@
-import React from "react";
-import "../../styles/staticFooter.scss";
-import { Grid } from "@material-ui/core";
+import React from 'react';
+import '../../styles/staticFooter.scss';
+import { Grid } from '@material-ui/core';
 import CopyrightIcon from '@material-ui/icons/Copyright';
+import { NavLink } from 'react-router-dom';
 
 export default function Footer() {
-  return (
-    <footer>
-      <Grid container justify="center" alignItems="center" className="foot-container">
-        <Grid item container direction="column" id="foot-content-grid">
-            <Grid item container>
-                <Grid item xs={3}><img src={require('../../img/twit.svg')} alt="Twitter logo"/></Grid>
-                <Grid item xs={3}><img src={require('../../img/face.svg')} alt="Facebook logo"/></Grid>
-                <Grid item xs={3}><img src={require('../../img/insta.png')} alt="Instagram logo"/></Grid>
-                <Grid item xs={3}><img src={require('../../img/youtube.svg')} alt="Youtube logo"/></Grid>
+   return (
+      <footer>
+         <Grid
+            container
+            justify='center'
+            alignItems='center'
+            className='foot-container'
+         >
+            <Grid item container direction='column' id='foot-content-grid'>
+               <Grid item container>
+                  <Grid item xs={3}>
+                     <img
+                        src={require('../../img/twit.svg')}
+                        alt='Twitter logo'
+                     />
+                  </Grid>
+                  <Grid item xs={3}>
+                     <img
+                        src={require('../../img/face.svg')}
+                        alt='Facebook logo'
+                     />
+                  </Grid>
+                  <Grid item xs={3}>
+                     <img
+                        src={require('../../img/insta.png')}
+                        alt='Instagram logo'
+                     />
+                  </Grid>
+                  <Grid item xs={3}>
+                     <img
+                        src={require('../../img/youtube.svg')}
+                        alt='Youtube logo'
+                     />
+                  </Grid>
+               </Grid>
+               <Grid item>
+                  <hr />
+               </Grid>
+               <Grid item>
+                  <p>
+                     <NavLink to='/about'>ABOUT US</NavLink>
+                  </p>
+               </Grid>
+               <Grid item>
+                  <hr />
+               </Grid>
+               <Grid item>
+                  <p>CONTACT US</p>
+               </Grid>
+               <Grid item>
+                  <hr />
+               </Grid>
+               <Grid
+                  item
+                  container
+                  justify='space-around'
+                  id='priv-terms-container'
+               >
+                  <Grid item xs={4}>
+                     <p>PRIVACY</p>
+                  </Grid>
+                  <Grid item xs={4}>
+                     <p>TERMS OF USE</p>
+                  </Grid>
+               </Grid>
+               <Grid item id='copyright-row'>
+                  <p>
+                     <CopyrightIcon fontSize='small' /> All rights reserved.
+                     Q-UP. 2020
+                  </p>
+               </Grid>
             </Grid>
-            <Grid item><hr/></Grid>
-            <Grid item>
-                <p>ABOUT US</p>
-            </Grid>
-            <Grid item><hr/></Grid>
-            <Grid item>
-                <p>CONTACT US</p>
-            </Grid>
-            <Grid item><hr/></Grid>
-            <Grid item container justify="space-around" id="priv-terms-container">
-                <Grid item xs={4}><p>PRIVACY</p></Grid>
-                <Grid item xs={4}><p>TERMS OF USE</p></Grid>
-            </Grid>
-            <Grid item id="copyright-row"><p><CopyrightIcon fontSize="small"/> All rights reserved. Q-UP. 2020</p></Grid>
-        </Grid>
-      </Grid>
-    
-    </footer>
-  );
+         </Grid>
+      </footer>
+   );
 }
