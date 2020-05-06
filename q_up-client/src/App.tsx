e651c334ba70f36705cf892a9d90061c08e4a171
TerryLun <tianweilun@yahoo.com>
2020-05-05 23:37:17 -0700

    Added about us route

--- q_up-client/src/App.tsx
+++ q_up-client/src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
-import {
-  BrowserRouter as Router, Route, Switch
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import LandingPage from './pages/landing';
 import LoginPage from './pages/login';
 import SignupPage from './pages/signup';
+import AboutUsPage from './pages/aboutUs';
 
 function App() {
    return (
@@ -15,6 +14,7 @@ function App() {
                <Route path='/' component={LandingPage} exact />
                <Route path='/login' component={LoginPage} exact />
                <Route path='/signup' component={SignupPage} exact />
+               <Route path='/about' component={AboutUsPage} exact />
             </Switch>
          </div>
       </Router>
