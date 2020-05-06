affb0f32f78d4f0f37db71b86b862f13db751620
Karel Chanivecky <Karl_Chanivecky@hotmail.com>
2020-05-06 12:46:00 -0700

    created mock data file

---
+++ q_up-client/src/mockData.tsx
@@ -0,0 +1,7 @@
+export function mockProvinces(){
+    return ["AB", "BC", "ON", "MB", "SK", "NF", "NB", "YK", "NW", "NV", "QC", "PEI"];
+}
+
+export function mockCategories(){
+    return ["Clinic", "Restaurant", "Government", "Grocer", "Hairdresser"];
+}
