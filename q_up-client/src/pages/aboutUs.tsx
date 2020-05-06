867bb6733ff1d9659de7e9160251c61ac9d768a8
TerryLun <tianweilun@yahoo.com>
2020-05-05 23:37:42 -0700

    Top image with text in about us page

--- q_up-client/src/pages/aboutUs.tsx
+++ q_up-client/src/pages/aboutUs.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import Footer from 'src/components/static/Footer';
 import Header from 'src/components/static/Header';
+import '../styles/aboutUs.scss';
+import { Grid } from '@material-ui/core';
+// import { Card, CardMedia } from '@material-ui/core';
 
 export default function aboutUsPage() {
-    return <>
-    <Header/>
-        <main>
-            about us page
-        </main>
-    <Footer/>
-    </>;
+   return (
+      <>
+         <Header />
+         <main>
+            <Grid
+               container
+               justify='center'
+               alignItems='center'
+               className='top-pic-section'
+            >
+               <Grid item xs={12}>
+                  <header id='top-img-text'>
+                     <h2>This is just the beginning.</h2>
+                  </header>
+               </Grid>
+            </Grid>
+         </main>
+         <Footer />
+      </>
+   );
 }
