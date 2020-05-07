b26076d85c77c96b80a4df6d23893ba752ff566f
TerryLun <tianweilun@yahoo.com>
2020-05-06 23:52:02 -0700

    Bring in provider and store

--- q_up-client/src/App.tsx
+++ q_up-client/src/App.tsx
@@ -4,29 +4,41 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import LandingPage from './pages/landing';
 import LoginPage from './pages/login';
 import SignupPage from './pages/signup';
-import aboutUsPage from "./pages/aboutUs";
+import aboutUsPage from './pages/aboutUs';
 import themes from './mui-theming/themes';
-import termsPage from "./pages/termsAndConditions";
-import privacyPage from "./pages/privaryPolicy";
+import termsPage from './pages/termsAndConditions';
+import privacyPage from './pages/privaryPolicy';
 import { ThemeProvider } from '@material-ui/core';
+import { Provider } from 'react-redux';
+import store from './store';
 
 function App() {
    const theme = themes();
    return (
-      <Router>
-         <div className='App'>
-            <ThemeProvider theme={theme}>
-            <Switch>
-               <Route path='/' component={LandingPage} exact />
-               <Route path='/login' component={LoginPage} exact />
-               <Route path='/signup' component={SignupPage} exact />
-               <Route path='/aboutUs' component={aboutUsPage} exact/>
-               <Route path='/termsAndConditions' component={termsPage} exact/>
-               <Route path='/PrivacyPolicy' component={privacyPage} exact/>
-            </Switch>
-            </ThemeProvider>
-         </div>
-      </Router>
+      <Provider store={store}>
+         <Router>
+            <div className='App'>
+               <ThemeProvider theme={theme}>
+                  <Switch>
+                     <Route path='/' component={LandingPage} exact />
+                     <Route path='/login' component={LoginPage} exact />
+                     <Route path='/signup' component={SignupPage} exact />
+                     <Route path='/aboutUs' component={aboutUsPage} exact />
+                     <Route
+                        path='/termsAndConditions'
+                        component={termsPage}
+                        exact
+                     />
+                     <Route
+                        path='/PrivacyPolicy'
+                        component={privacyPage}
+                        exact
+                     />
+                  </Switch>
+               </ThemeProvider>
+            </div>
+         </Router>
+      </Provider>
    );
 }
 
