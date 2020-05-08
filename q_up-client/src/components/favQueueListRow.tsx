453179094667493c1f28a5a25f3c4696530a4f6f
Karel Chanivecky <Karl_Chanivecky@hotmail.com>
2020-05-07 19:58:39 -0700

    completed layout

--- q_up-client/src/components/favQueueListRow.tsx
+++ q_up-client/src/components/favQueueListRow.tsx
@@ -4,14 +4,32 @@ import {
   ExpansionPanelDetails,
   Grid,
   Typography,
+  IconButton,
+  Button,
 } from "@material-ui/core";
+import { ExpandMore } from "@material-ui/icons";
+import StarIcon from "@material-ui/icons/Star";
+import { mockFavQueues } from "src/mockData";
 
 export default function FavQueueListRow(props: any) {
   const data = props.data;
+  const address = data.address;
+  const unit =
+    address.unit === "" ? (
+      <div></div>
+    ) : (
+      <Grid container item xs={12}>
+        <Typography variant="body1">unit</Typography>
+        <Typography variant="body2">{address.unit}</Typography>
+      </Grid>
+    );
+  const queueUp = () ={
+
+  };
   return (
     <>
       <ExpansionPanelSummary
-        expandIcon={<ExpandMoreIcon />}
+        expandIcon={<ExpandMore />}
         aria-controls="panel1bh-content"
         id="panel1bh-header"
       >
@@ -28,9 +46,58 @@ export default function FavQueueListRow(props: any) {
             <Typography variant="caption">Queue Size</Typography>
             <Typography variant="body2">{data.size}</Typography>
           </Grid>
+          <Grid item xs={2}>
+            <IconButton color="secondary">
+              <StarIcon />
+            </IconButton>
+          </Grid>
         </Grid>
       </ExpansionPanelSummary>
-      <ExpansionPanelDetails>{/* the body */}</ExpansionPanelDetails>
+      <ExpansionPanelDetails>
+        {/* the body */}
+        <Grid container>
+          <Grid container item xs={8}>
+            {" "}
+            {/* address*/}
+            <address>
+              <Typography variant="h3">Address</Typography>
+              {unit}
+              <Grid item xs={12}>
+                <Typography variant="body1">Street address</Typography>
+                <Typography variant="body2">{address.streetAddress}</Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="body1">City</Typography>
+                <Typography variant="body2">{address.city}</Typography>
+              </Grid>
+              <Grid item xs={4}>
+                <Typography variant="body1">Province</Typography>
+                <Typography variant="body2">{address.province}</Typography>
+              </Grid>
+              <Grid item xs={8}>
+                <Typography variant="body1">Postal Code</Typography>
+                <Typography variant="body2">{address.postalCode}</Typography>
+              </Grid>
+            </address>
+          </Grid>
+          <Grid container item xs={4}>
+            <Grid item xs={12}>
+              <Typography variant="body2">{data.phoneNumber}</Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="body2">{data.email}</Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="body2">{data.website}</Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Button variant="contained" color="primary" onClick={queueUp}>
+                Queue up
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </ExpansionPanelDetails>
     </>
   );
 }
