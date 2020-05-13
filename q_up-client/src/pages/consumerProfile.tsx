b599f63e0f41aab4de30d833a377b909b5da9afa
Karel Chanivecky <Karl_Chanivecky@hotmail.com>
2020-05-13 11:35:56 -0700

    consumer profile complete, may need debugging

--- q_up-client/src/pages/consumerProfile.tsx
+++ q_up-client/src/pages/consumerProfile.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 // import { Link } from 'react-router-dom';
 import Footer from "../components/static/Footer";
 import Header from "../components/static/Header";
 import ConsumerNav from "../components/consumerNav";
-import { Grid, Button } from "@material-ui/core";
+import { Grid, Button, Typography, makeStyles } from "@material-ui/core";
+import Axios from "axios";
+import {formatNumber} from '../utils/formatting';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   button: {
     margin: "20px auto 20px auto",
   },
@@ -13,6 +15,25 @@ const useStyles = makeStyles((theme) => ({
 
 export default function ConsumerProfilePage() {
   const classes = useStyles();
+  const [email, setEmail] = useState("some@email.ca");
+  const [phoneNumber, setPhoneNumber] = usestate("(778)898-9898");
+  const [postalCode, setPostalCode] = usestate("");
+  const axiosConfig = {
+    headers: {
+      Authorization: `Bearer ${JSON.parse(sessionStorage.user).token}`,
+    },
+  };
+
+  Axios.get("/customerInfo", axiosConfig)
+  .then((data:any) => {
+    setEmail(data.email);
+    setPhoneNumber(formatNumber(data.phoneNumber));
+    setPostalCode(data.postalCode);
+  })
+  .catch(err => {
+    window.alert("Connection error");
+    console.log(err);
+  });
   return (
     <>
       <Header Nav={ConsumerNav} />
@@ -20,7 +41,7 @@ export default function ConsumerProfilePage() {
         <section>
           <Grid container justify="space-around">
             <Grid item xs={12} md={4}>
-            <Button
+              <Button
                 variant="contained"
                 color="primary"
                 className={classes.button}
@@ -29,7 +50,7 @@ export default function ConsumerProfilePage() {
               </Button>
             </Grid>
             <Grid item xs={12} md={4}>
-            <Button
+              <Button
                 variant="contained"
                 color="primary"
                 className={classes.button}
@@ -38,7 +59,7 @@ export default function ConsumerProfilePage() {
               </Button>
             </Grid>
             <Grid item xs={12} md={4}>
-            <Button
+              <Button
                 variant="contained"
                 color="primary"
                 className={classes.button}
@@ -49,7 +70,26 @@ export default function ConsumerProfilePage() {
           </Grid>
         </section>
         <section>
-
+          <Grid container>
+            <Grid item sm={12} md={6}>
+              <Typography variant="body1">Email</Typography>
+            </Grid>
+            <Grid item sm={12} md={6}>
+              <Typography variant="body2">{email}</Typography>
+            </Grid>
+            <Grid item sm={12} md={6}>
+              <Typography variant="body1">Phone number</Typography>
+            </Grid>
+            <Grid item sm={12} md={6}>
+              <Typography variant="body2">{phoneNumber}</Typography>
+            </Grid>
+            <Grid item sm={12} md={6}>
+              <Typography variant="body1">Postal code</Typography>
+            </Grid>
+            <Grid item sm={12} md={6}>
+              <Typography variant="body2">{postalCode}</Typography>
+            </Grid>
+          </Grid>
         </section>
       </main>
       <Footer />
