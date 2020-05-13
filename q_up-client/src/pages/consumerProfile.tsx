9e42330bd35857e4268ad6df64296e23d42decc5
Karel Chanivecky <Karl_Chanivecky@hotmail.com>
2020-05-13 11:35:29 -0700

    consumer profile complete. may need debugging

--- q_up-client/src/pages/consumerProfile.tsx
+++ q_up-client/src/pages/consumerProfile.tsx
@@ -3,12 +3,55 @@ import React from "react";
 import Footer from "../components/static/Footer";
 import Header from "../components/static/Header";
 import ConsumerNav from "../components/consumerNav";
+import { Grid, Button } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    margin: "20px auto 20px auto",
+  },
+}));
 
 export default function ConsumerProfilePage() {
+  const classes = useStyles();
   return (
     <>
       <Header Nav={ConsumerNav} />
-      <main>consumer profile page</main>
+      <main>
+        <section>
+          <Grid container justify="space-around">
+            <Grid item xs={12} md={4}>
+            <Button
+                variant="contained"
+                color="primary"
+                className={classes.button}
+              >
+                Edit profile
+              </Button>
+            </Grid>
+            <Grid item xs={12} md={4}>
+            <Button
+                variant="contained"
+                color="primary"
+                className={classes.button}
+              >
+                Delete account
+              </Button>
+            </Grid>
+            <Grid item xs={12} md={4}>
+            <Button
+                variant="contained"
+                color="primary"
+                className={classes.button}
+              >
+                Change password
+              </Button>
+            </Grid>
+          </Grid>
+        </section>
+        <section>
+
+        </section>
+      </main>
       <Footer />
     </>
   );
