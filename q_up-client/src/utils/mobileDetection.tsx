3156c8e6011d0c46d919e89a9e3935e2e4e59ea2
Karel Chanivecky <Karl_Chanivecky@hotmail.com>
2020-05-19 10:27:01 -0700

    commented mobile detection

--- q_up-client/src/utils/mobileDetection.tsx
+++ q_up-client/src/utils/mobileDetection.tsx
@@ -1,3 +1,11 @@
+
+/**
+ * Detect if the app is being run in mobile or web.
+ * 
+ * returns true if is mobile else false
+ * 
+ * Credits: patrick_mcwilli, Tealium Learning Center, accessed: May 16 2020, https://community.tealiumiq.com/t5/Developers/How-do-you-build-a-load-rule-for-mobile-only-devices/td-p/5282
+ */
 export default function isMobile() {
   if (
     /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(
