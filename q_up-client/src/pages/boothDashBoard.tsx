8de7da6441cfab05b307dbd50ca67f6b01cbfdbc
TerryLun <tianweilun@yahoo.com>
2020-05-19 13:05:27 -0700

    Made enter name a separate component.

--- q_up-client/src/pages/boothDashBoard.tsx
+++ q_up-client/src/pages/boothDashBoard.tsx
@@ -1,19 +1,44 @@
 import React from 'react';
 import Footer from '../components/static/Footer';
 import Header from '../components/static/Header';
-import { TextField } from '@material-ui/core';
+import BoothEnterName from '../components/boothEnterName';
+import { Grid, Typography } from '@material-ui/core';
 
 export default function BoothDashBoard() {
    return (
       <>
          <Header />
          <main>
-            <TextField
-               color='secondary'
-               id='phone'
-               label='Enter Your name'
-               name='name'
-            />
+            <BoothEnterName />
+            <Grid container spacing={8} justify='center'>
+               <Grid item xs={3}>
+                  <Typography>Feature</Typography>
+                  <Typography>
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                     Etiam suscipit ipsum felis, in finibus libero tincidunt
+                     nec. Pellentesque sit amet rutrum urna. Interdum et
+                     malesuada fames ac ante ipsum primis in faucibus.
+                  </Typography>
+               </Grid>
+               <Grid item xs={3}>
+                  <Typography>Feature</Typography>
+                  <Typography>
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                     Etiam suscipit ipsum felis, in finibus libero tincidunt
+                     nec. Pellentesque sit amet rutrum urna. Interdum et
+                     malesuada fames ac ante ipsum primis in faucibus.
+                  </Typography>
+               </Grid>
+               <Grid item xs={3}>
+                  <Typography>Feature</Typography>
+                  <Typography>
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                     Etiam suscipit ipsum felis, in finibus libero tincidunt
+                     nec. Pellentesque sit amet rutrum urna. Interdum et
+                     malesuada fames ac ante ipsum primis in faucibus.
+                  </Typography>
+               </Grid>
+            </Grid>
          </main>
          <Footer />
       </>
