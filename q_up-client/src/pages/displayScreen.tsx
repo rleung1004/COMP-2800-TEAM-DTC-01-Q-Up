42dd99963a3301ed33621795689d8ca7e69ac76d
TerryLun <tianweilun@yahoo.com>
2020-05-20 17:51:38 -0700

    Created display screen page.

---
+++ q_up-client/src/pages/displayScreen.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import DisplayHeader from '../components/displayHeader';
+import DisplayFooter from '../components/displayFooter';
+import { Grid, Box, Typography } from '@material-ui/core';
+import '../styles/displayScreen.scss';
+
+export default function BoothDashBoard() {
+   return (
+      <>
+         <DisplayHeader />
+         <main>
+            <Grid container>
+               <Grid item xs={7}>
+                  <Box pt={20}>
+                     <Typography className='now-serving'>
+                        NOW SERVING
+                     </Typography>
+                  </Box>
+                  <Box pb={10}>
+                     <Typography className='now-serving-ticket-number'>
+                        #154
+                     </Typography>
+                  </Box>
+               </Grid>
+               <Grid className='right-columns' item xs={5} container>
+                  <Grid xs={6}>
+                     <Box pt={5}>Ticket #</Box>
+                  </Grid>
+                  <Grid xs={6}>
+                     <Box pt={5}>Status</Box>
+                  </Grid>
+               </Grid>
+            </Grid>
+         </main>
+         <DisplayFooter />
+      </>
+   );
+}
